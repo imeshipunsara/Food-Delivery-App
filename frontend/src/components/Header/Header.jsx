895d241209151cbf9ff2474d1257c5935bf71cfc
Imeshi Punsara <imeshipunsara9@gmail.com>
2026-03-28 19:36:31 +0530

    Create Header section

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Your Favorite food here</h2>
+        <p>Choose from a diverse nenu faturing a delectable array of dishes with the finest ingredient and culinary expertise. our delicios mal at a time.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
