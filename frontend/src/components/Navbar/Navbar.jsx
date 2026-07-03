5cfaafaec0bdc16a35228bb1e2a84332eb6130f1
Imeshi Punsara <imeshipunsara9@gmail.com>
2026-07-03 19:52:25 +0530

    last update on today

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../assets/assets.js'
 import { Link } from 'react-router-dom'
 
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
   //undeline clicking rhe menu item
   const [menu,setmenu] = useState("home");
 
@@ -23,7 +23,7 @@ const Navbar = () => {
             <img src={assets.basket_icon} alt="" />
             <div className='dot'></div>
           </div>
-          <button>Sign in</button>
+          <button onClick={()=>setShowLogin(true)}>Sign in</button>
       </div>
     </div>
   )
